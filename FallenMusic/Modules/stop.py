014b91c336a0f7f144d9e1264124b92ee24e5937
vidroasaac <148828173+Asaaq-ali@users.noreply.github.com>
2023-11-02 18:50:55 +0300

    Update stop.py

--- FallenMusic/Modules/stop.py
+++ FallenMusic/Modules/stop.py
@@ -27,7 +27,7 @@ from FallenMusic import app, pytgcalls
 from FallenMusic.Helpers import _clear_, admin_check, close_key
 
 
-@app.on_message(filters.command(["stop", "end"]) & filters.group)
+@app.on_message(filters.command(["ايقاف", "انهاء"],"") & filters.group)
 @admin_check
 async def stop_str(_, message: Message):
     try:
@@ -41,6 +41,6 @@ async def stop_str(_, message: Message):
         pass
 
     return await message.reply_text(
-        text=f"➻ **sᴛʀᴇᴀᴍ ᴇɴᴅᴇᴅ/sᴛᴏᴩᴩᴇᴅ** ❄\n│ \n└ʙʏ : {message.from_user.mention} 🥀",
+        text=f"✧ **تم إنهاء التشــغيل**\n✧ ** بواسطة :** {message.from_user.mention} ",
         reply_markup=close_key,
     )
