0ea7c279e9da2c3b0e1bbe4eb01cf331ceefd9c6
vidroasaac <148828173+Asaaq-ali@users.noreply.github.com>
2023-11-02 19:08:08 +0300

    Update pause.py

--- FallenMusic/Modules/pause.py
+++ FallenMusic/Modules/pause.py
@@ -27,7 +27,7 @@ from FallenMusic import app, pytgcalls
 from FallenMusic.Helpers import admin_check, close_key, is_streaming, stream_off
 
 
-@app.on_message(filters.command(["pause"]) & filters.group)
+@app.on_message(filters.command(["قف","توقف","جنى اص"],""))
 @admin_check
 async def pause_str(_, message: Message):
     try:
@@ -37,12 +37,12 @@ async def pause_str(_, message: Message):
 
     if not await is_streaming(message.chat.id):
         return await message.reply_text(
-            "ᴅɪᴅ ʏᴏᴜ ʀᴇᴍᴇᴍʙᴇʀ ᴛʜᴀᴛ ʏᴏᴜ ʀᴇsᴜᴍᴇᴅ ᴛʜᴇ sᴛʀᴇᴀᴍ ?"
+            ".."
         )
 
     await pytgcalls.pause_stream(message.chat.id)
     await stream_off(message.chat.id)
     return await message.reply_text(
-        text=f"➻ sᴛʀᴇᴀᴍ ᴩᴀᴜsᴇᴅ 🥺\n│ \n└ʙʏ : {message.from_user.mention} 🥀",
+        text=f"✧ **تم ايقاف التشــغيل مؤقتاً  ** \n ✧** بواسطــة ** : {message.from_user.mention} ",
         reply_markup=close_key,
     )
